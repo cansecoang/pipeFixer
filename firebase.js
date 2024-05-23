06c0bbd26bc8651bdecdfb1a732a746bf0e6c7da
Yammm8 <yamir.jb@gmail.com>
2024-05-23 16:34:28 -0600

    inserte las librerias

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { } from 'firebase/auth';
+import { } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
